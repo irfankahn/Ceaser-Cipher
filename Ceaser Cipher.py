ffc3f835b3bf194a037c49912f3736a5e3318ae7
Irfanullah Khan <79622197+irfankahn@users.noreply.github.com>
2021-02-25 00:28:31 -0500

    Add files via upload

---
+++ Ceaser Cipher.py
@@ -0,0 +1,96 @@
+
+
+# We will solve this problem in 2 steps #
+# (1) Using only one function
+# (2) Using two functions, one for encoding and another for decoding
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd',
+            'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z']
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+# This is the simplest solution using only one function #
+def ceasar(plain_text, shift_amount):
+    cipher_text = ""
+    if direction == "encode":
+        for letter in plain_text:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        print(f"The encoded text is {cipher_text}")
+    elif direction == "decode":
+        cipher_text = ""
+        for letter in plain_text:
+            position = alphabet.index(letter)
+            new_position = position - shift_amount
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        print(f"The decoded text is {cipher_text}")
+
+ceasar(plain_text=text, shift_amount=shift)
+
+
+
+# This solution is for using 2 functions #
+
+# def encrypt(plain_text, shift_amount):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         new_letter = alphabet[new_position]
+#         cipher_text += new_letter
+#     print(f"The encoded text is {cipher_text}")
+#
+#
+#
+# def decrypt(plain_text, shift_amount):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position - shift_amount
+#         new_letter = alphabet[new_position]
+#         cipher_text += new_letter
+#     print(f"The decoded text is {cipher_text}")
+#
+#
+# if direction == "encode":
+#     encrypt(plain_text=text, shift_amount=shift)
+#
+# elif direction == "decode":
+#     decrypt(plain_text=text, shift_amount=shift)
+#
+# else:
+#     print("Invalid input")
+
+
+
+
